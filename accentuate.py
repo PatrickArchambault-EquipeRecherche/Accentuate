92a070d84e5f8f27ebcce6c8d961bde5c05f3ce5
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2022-02-09 16:43:01 -0500

    Trying a rough-and-ready approach...

--- accentuate.py
+++ accentuate.py
@@ -8,7 +8,19 @@ then write it to a new CSV file where all of the data is UTF8, including
 any accented characters, curly quotes, or other weirdness.
 """
 
-import os       # For file inspection
-import sys      # For command-line arguments
-import csv      # For reading and writing CSV files
+import os                   # For file inspection
+import sys                  # For command-line arguments
+import csv                  # For reading and writing CSV files
+import charset_normalizer   # For converting encodings
+
+input_filename = "test.csv"
+output_filename = "results.csv"
+
+input_file_size = os.path.getsize(input_filename)
+
+print(input_file_size)
+
+with open(input_filename, "rb") as inf, open(output_filename, "wb") as outf:
+    rslts = charset_normalizer.from_fp(inf).best()
+    print(rslts)
 
