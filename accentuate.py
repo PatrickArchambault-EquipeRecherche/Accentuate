527f8d6e33bea8fba648720cbd0f0bd6b74d1913
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2022-02-10 12:31:55 -0500

    Switched to ftfy, going cell-by-cell

--- accentuate.py
+++ accentuate.py
@@ -8,10 +8,11 @@ then write it to a new CSV file where all of the data is UTF8, including
 any accented characters, curly quotes, or other weirdness.
 """
 
+from distutils.dep_util import newer_group
 import os                   # For file inspection
 import sys                  # For command-line arguments
 import csv                  # For reading and writing CSV files
-import charset_normalizer   # For converting encodings
+import ftfy   # For converting encodings
 
 input_filename = "test.csv"
 output_filename = "results.csv"
@@ -20,7 +21,15 @@ input_file_size = os.path.getsize(input_filename)
 
 print(input_file_size)
 
-with open(input_filename, "rb") as inf, open(output_filename, "wb") as outf:
-    rslts = charset_normalizer.from_fp(inf).best()
-    print(rslts)
+with open(input_filename, "r", newline="") as inf, \
+     open(output_filename, "w", newline="") as outf:
+    inreader = csv.reader(inf, delimiter=";")
+    outwriter = csv.writer(outf)
+    for row in inreader:
+        newrow = []
+        for cell in row:
+            print(cell)
+            newrow.append(ftfy.fix_text(cell))
+        outwriter.writerow(newrow)
+
 
