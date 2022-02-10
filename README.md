adb6d0ae3ee56a86c6ac522a8718f3d76c6d73b6
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2022-02-10 14:34:37 -0500

    Adding commandline interface, updating readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Converting and harmonizing CSV files with accents
 
 ## Purpose
-It is easy to have CSV (Comma Separated Values) files with *special* characters, but not always easy to read them, or move their data from place to place without losing data due to these accents.  Sometimes, the results are subtle, and you won't know that you lost anything until much later.
+It is easy to have CSV (Comma Separated Values) files with *special* characters, but not always easy to read them, or move their data from place to place without losing data due to these characters.  Sometimes, the results are subtle, and you won't know that you lost anything until much later.
 
 *Special* characters may be accents, but they also include punctuation, such as "curly" apostrophes and quote marks.  It is easy to get text data with incompatible characters from one field to another, but hard to reconcile manually.
 
--- accentuate.py
+++ accentuate.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python
 
 """
-Take a CSV file, check if it is big enough yoink into memory (choosing a 
-configurable value for how large that is), or planning to take it in chunks, 
-scan it for which character set it is probably in, then convert to UTF8, 
-then write it to a new CSV file where all of the data is UTF8, including 
-any accented characters, curly quotes, or other weirdness.
+Take a CSV file, read it row by row and cell by cell. Convert each cell to
+UTF8 with ftfy, and at the end of each row, write the converted row out to
+a new file.
 """
 
-from distutils.dep_util import newer_group
-import os                   # For file inspection
-import sys                  # For command-line arguments
-import csv                  # For reading and writing CSV files
+import os     # For file inspection
+import sys    # For command-line arguments
+import csv    # For reading and writing CSV files
 import ftfy   # For converting encodings
 
 input_filename = "test.csv"
 output_filename = "results.csv"
 
-input_file_size = os.path.getsize(input_filename)
+if len(sys.argv) > 1:
+    #print("Arg!")
+    if sys.argv[1]:
+        input_filename = sys.argv[1]
+    if sys.argv[2]:
+        if os.path.getsize(sys.argv[2]) > 0:
+            print(f"Warning: {sys.argv[2]} exists!")
+            exit()
+        else:
+            output_filename = sys.argv[2]
+    #print(input_file_size)
 
-print(input_file_size)
+def fixAllTheCells(input_filename, output_filename):
+    with open(input_filename, "r", newline="") as inf, \
+         open(output_filename, "w", newline="") as outf:
+        inreader = csv.reader(inf, delimiter=";")
+        outwriter = csv.writer(outf)
+        for row in inreader:
+            newrow = []
+            for cell in row:
+                print(cell)
+                newrow.append(ftfy.fix_text(cell))
+            outwriter.writerow(newrow)
 
-with open(input_filename, "r", newline="") as inf, \
-     open(output_filename, "w", newline="") as outf:
-    inreader = csv.reader(inf, delimiter=";")
-    outwriter = csv.writer(outf)
-    for row in inreader:
-        newrow = []
-        for cell in row:
-            print(cell)
-            newrow.append(ftfy.fix_text(cell))
-        outwriter.writerow(newrow)
-
-
+fixAllTheCells(input_filename, output_filename)
